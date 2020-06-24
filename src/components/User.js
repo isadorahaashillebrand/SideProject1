351716cafc6ca2fc3da39c0fdbf065349b0bf9dd
isadorahaashillebrand <isadorahaas@gmail.com>
2020-06-24 21:45:06 +0100

    secondLevel

--- src/components/User.js
+++ src/components/User.js
@@ -10,30 +10,38 @@ class User extends React.Component {
     };
   }
 
-componentDidMount() {
+  componentDidMount() {
     // Send the request
     axios
       .get("https://jsonplaceholder.typicode.com/users")
       // Extract the DATA from the received response
-      .then(response => {
+      .then((response) => {
         return response.data;
       })
       // Use this data to update the state
-      .then(dataresult => {
+      .then((dataresult) => {
         console.log(dataresult);
-        this.setState({user: dataresult});
+        this.setState({ user: dataresult });
       });
+  }
+
+  deleteUser = (id) => {
+    const users = this.state.user;
+    let deleteUser = users.filter((user) => user.id !== id);
+    this.setState({ user : deleteUser })
   }
 
   render() {
     return (
       <div className="User">
-          {this.state.user.map(person => (
-            <UserCard user={person}/>
-          ))}
-        </div>
+        {this.state.user.map((person) => (
+          <div className="inputUser">
+            <UserCard key={person.id} user={person} deleteUser={this.deleteUser}/>
+          </div>
+        ))}
+      </div>
     );
-  };
+  }
 }
 
 export default User;
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,49 +1,94 @@
-import React from 'react';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import "./UserCard.css";
 
 class UserCard extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        id: this.props.user.id,
-        username: this.props.user.username,
-        name: this.props.user.name,
-        email: this.props.user.email,
-        street: this.props.user.address.street,
-        city: this.props.user.address.city,
-        company: this.props.user.company,
-        website: this.props.user.website,
-        phone: this.props.user.phone
-      }
-    }
-  
-    render() {
-      return (
-        <div className="User">
-          <div className="username bold">User {this.state.id} - {this.state.username}</div>
-          <div className="name space">{this.state.name}</div>
-          <div className="subject">
-            Email: 
-            <span className="text"> {this.state.email}</span>
-          </div>
-          <div className="subject">
-            Adress: 
-            <span className="text"> {this.state.street}, {this.state.city}</span>
-          </div>
-          <div className="subject space">
-            Phone: 
-            <span className="text"> {this.state.phone}</span>
-          </div>
-          <div className="subject">
-            Company: 
-            <span className="text"> {this.state.company.name}</span>
-          </div>
-          <div className="subject">
-            Website: 
-            <span className="text"> {this.state.website}</span>
-          </div>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.user.id,
+      username: this.props.user.username,
+      name: this.props.user.name,
+      email: this.props.user.email,
+      street: this.props.user.address.street,
+      city: this.props.user.address.city,
+      company: this.props.user.company.name,
+      website: this.props.user.website,
+      phone: this.props.user.phone,
+    };
   }
-  
-  export default UserCard;
+
+  render() {
+    const {
+      id,
+      username,
+      street,
+      name,
+      email,
+      city,
+      company,
+      website,
+      phone,
+    } = this.state;
+    return (
+      <div>
+        <div className="UserCard">
+          <Card>
+            <div className="input">
+              <Card.Text>
+                <spam className="title">Id User:</spam> {id}
+              </Card.Text>
+            </div>
+            <div className="input">
+              <Card.Text>
+                <spam className="title">Name:</spam> {name}
+              </Card.Text>
+            </div>
+            <div className="input">
+              <Card.Text>
+                <spam className="title">UserName:</spam> {username}
+              </Card.Text>
+            </div>
+            <div className="input">
+              <Card.Text>
+                <spam className="title">Contact:</spam>
+                <ul className="contact">
+                  <li>
+                    <spam className="title2">Phone:</spam> {phone}
+                  </li>
+                  <li>
+                    <spam className="title2">E-mail:</spam> {email}
+                  </li>
+                  <li>
+                    <spam className="title2">Website:</spam> {website}
+                  </li>
+                </ul>
+              </Card.Text>
+            </div>
+            <div className="input">
+              <Card.Text>
+                <spam className="title">Address:</spam>
+                <ul className="address">
+                  <li>
+                    <spam className="title2">City:</spam> {city}
+                  </li>
+                  <li>
+                    <spam className="title2">Street:</spam> {street}
+                  </li>
+                </ul>
+              </Card.Text>
+            </div>
+            <div className="input">
+              <Card.Text>
+                <spam className="title">Company Name:</spam> {company}
+              </Card.Text>
+            </div>
+            <Button onClick={() => this.props.deleteUser(id)}>Delete User</Button>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserCard;
